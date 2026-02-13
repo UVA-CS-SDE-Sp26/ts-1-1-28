f3ead7026b47663bd2b7e751a0675d68e0dac4d8
lbj3midec <aj3of5@gmail.com>
2026-02-13 15:24:46 -0500

    finish ProgramControl, import Mockito

---
+++ src/test/java/topsecret/ProgramControlTest.java
@@ -0,0 +1,21 @@
+package topsecret;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+
+public class ProgramControlTest {
+    /*
+    @BeforeEach
+    void setup() {
+        FileHandler fh = mock(FileHandler.class);
+        ProgramControl pc = new ProgramControl(fh);
+    }
+
+    @Test
+    public void noArgsShortTest() {
+        pc.handleArgs();
+    }
+    */
+}
